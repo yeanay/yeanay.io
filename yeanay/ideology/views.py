d5349e1acc83cc36d011bdb818bfd1dc7c5c1913
Chris Brown <chrbrown@sas.upenn.edu>
2013-04-18 19:30:50 -0400

    Fixed problem with not accessing values correctly

--- yeanay/ideology/views.py
+++ yeanay/ideology/views.py
@@ -8,9 +8,9 @@ def congress(request, congress_num, cham):
     json_dict = []
     print len(full_congress)
     for mem in full_congress:
-        json_dict.append({'name': mem['name'],
-                         'district': mem['district'],
-                         'party_id': mem['party_id'],
-                         'state_id': mem['state_id'],
-                         'congress': mem['congress']})
+        json_dict.append({'name': mem.name,
+                         'district': mem.district,
+                         'party_id': mem.party_id,
+                         'state_id': mem.state_id,
+                         'congress': mem.congress})
     return HttpResponse(json.dumps(json_dict))
