006da16232a6ad6b8dd024245353bd60b1bd294b
Chris Brown <chrbrown@sas.upenn.edu>
2013-04-18 20:37:09 -0400

    Added congress number and actual ideology score

--- yeanay/ideology/views.py
+++ yeanay/ideology/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from ideology.models import ideology
 
 import json
+
 def congress(request, congress_num, cham):
     full_congress = ideology.objects.filter(congress=congress_num,
                     chamber=cham)
@@ -12,5 +13,6 @@ def congress(request, congress_num, cham):
                          'district': mem.district,
                          'party_id': mem.party_id,
                          'state_id': mem.state_id,
-                         'congress': mem.congress})
+                         'congress': mem.congress,
+                         'ideology': mem.first_dimension})
     return HttpResponse(json.dumps(json_dict))
