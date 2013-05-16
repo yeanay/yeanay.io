3861c8da0793e0f458fc4d3dddfe13adf48feef0
Chris Brown <chrbrown@sas.upenn.edu>
2013-05-16 16:20:21 -0400

    Outline of code to load bioguides for Congresses

--- yeanay/ideology/management/commands/load_bios.py
+++ yeanay/ideology/management/commands/load_bios.py
@@ -1,1 +1,23 @@
-# Load Legislator Bios #
+from django.core.management.base import BaseCommand, CommandError
+from ideology.models import ideology
+from django.conf import settings
+
+
+from lxml import etree as et
+
+class Command(BaseCommand):
+    def list_bio_xml():
+        """Lists bioguide XML files from govtrack"""
+        pass
+
+    def parse_xml():
+        """Parses a given XML file into a list of
+        dictionaries"""
+        pass
+
+    def load_to_db():
+        """Loads a python dictionary of bio data into
+        database"""
+        pass
+
+tree = et.parse('./data/bios/')
