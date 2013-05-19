ebcc4df8f03bc78e9f7ae2b9778798de54fa4b23
Chris Brown <chrbrown@sas.upenn.edu>
2013-05-19 15:15:55 -0400

    Now uses foreign keys for legislators

--- yeanay/ideology/management/commands/load_ideology.py
+++ yeanay/ideology/management/commands/load_ideology.py
@@ -1,7 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
-from ideology.models import ideology
+from ideology.models import Ideology, Legislator
 from django.conf import settings
-
 import csv
 
 class Command(BaseCommand):
@@ -18,26 +17,47 @@ class Command(BaseCommand):
         Helper function to load CSVs into DB
         """
         for obs in csv_reader:
-            if obs['congress'] < 80:
-                continue
-            leg = ideology()
-            leg.congress = obs['congress']
-            leg.icpsr_id = obs['icpsr_id']
-            leg.state_id = obs['state_code']
-            leg.district = obs['cd']
-            leg.name = obs['name']
-            leg.party_id = obs['party_code']
-            leg.first_dimension = obs['first_dimension']
-            leg.second_dimension = obs['second_dimension']
-            leg.chamber = chamber
-            leg.save()
+            if int(obs['congress']) < 82:
+                pass
+            elif int(obs['cd']) == 0 and obs['state'] == 'USA':
+                pass
+            else:
+                new_ideo = Ideology()
+                new_ideo.congress = obs['congress']
+                new_ideo.icpsrid = obs['icpsr_id']
+                new_ideo.state_id = obs['state_code']
+                new_ideo.district = obs['cd']
+                new_ideo.name = obs['name']
+                new_ideo.party_id = obs['party_code']
+                new_ideo.first_dimension = obs['first_dimension']
+                new_ideo.second_dimension = obs['second_dimension']
+                new_ideo.chamber = chamber
+                try:
+                    legis = Legislator.objects.get(icpsrid=obs['icpsr_id'])
+                    new_ideo.legislator = legis
+                    new_ideo.save()
+                except Legislator.DoesNotExist:
+                    print "\nCould not find Legislator using ICPSR {0}".format(obs['icpsr_id'])
+
+                    last_name = obs['name'].split(' ')[0].title()
+
+                    print "Trying Last Name Search on {0}".format(last_name)
+                    try:
+                        legis = Legislator.objects.get(lastname=last_name)
+                        new_ideo.legislator = legis
+                        new_ideo.save()
+                    except (Legislator.MultipleObjectsReturned, Legislator.DoesNotExist):
+                        print "Multiple Legislators with same last name"
+                        print "NAME: {0}".format(obs['name'])
+                except Legislator.MultipleObjectsReturned:
+                    print "Multiple legislators with icpsr id: {0}".format(obs['icpsr_id'])
             
     def handle(self, *args, **options):
         if len(args) != 0:
             raise CommandError('No arguments taken for command')
 
         # Delete existing ideology entries #
-        ideology.objects.all().delete()
+        Ideology.objects.all().delete()
 
         # Read in CSV file from Data Folder #
         house_file = open('./data/house_ideology.csv', 'r')
